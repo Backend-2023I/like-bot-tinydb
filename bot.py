ef333acdfe8593a7b99a0edcc31a3520d5b24b13
JavohirJalilov <javohirjaliloff@gmail.com>
2023-09-11 14:52:12 +0500

    photo

--- bot.py
+++ bot.py
@@ -14,13 +14,47 @@ def start(update: Update, context: CallbackContext):
 
     bot.sendMessage(chat_id, "Welcome!")
 
+def photo(update: Update, context: CallbackContext):
+
+    bot = context.bot
+    chat_id = update.message.chat.id
+
+    likes = db.all_likes()
+    dislikes = db.all_dislikes()
+
+    like = InlineKeyboardButton(text=f'👍 {likes}', callback_data='like')
+    dislike = InlineKeyboardButton(text=f'👎 {dislikes}', callback_data='dislike')
+
+    keyboard = InlineKeyboardMarkup([[like, dislike]])
+    photo = update.message.photo[0].file_id
+    bot.sendPhoto(chat_id, photo, reply_markup=keyboard)
+
 def main(update: Update, context: CallbackContext):
-    pass
+    query = update.callback_query
+    data = query.data
+
+    chat_id = query.message.chat.id
+
+    if data == 'like':
+        db.add_like(chat_id)
+    if data == "dislike":
+        db.add_dislike(chat_id)
+
+    likes = db.all_likes()
+    dislikes = db.all_dislikes()
+
+    like = InlineKeyboardButton(text=f'👍 {likes}', callback_data='like')
+    dislike = InlineKeyboardButton(text=f'👎 {dislikes}', callback_data='dislike')
+
+    keyboard = InlineKeyboardMarkup([[like, dislike]])
+
+    query.edit_message_reply_markup(reply_markup=keyboard)
 
 updater = Updater(token=TOKEN)
 dp = updater.dispatcher
 
 dp.add_handler(CommandHandler('start', start))
+dp.add_handler(MessageHandler(Filters.photo, photo))
 dp.add_handler(CallbackQueryHandler(main))
 
 updater.start_polling()
